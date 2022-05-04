b131683d5b006c0c7f155a446780b41e39f07fea
joaquinrmi <joaquinruaimi@gmail.com>
2022-05-04 15:46:17 -0300

    Add SearchQuery interface

---
+++ src/model/search_query.ts
@@ -0,0 +1,13 @@
+import BasicDocument from "./basic_document";
+import QueryFilter from "./query_filter";
+import SearchOrder from "./search_order";
+
+interface SearchQuery<DocumentType extends BasicDocument>
+{
+    orderBy?: SearchOrder<DocumentType> | SearchOrder<DocumentType>[];
+    limit?: number;
+    offset?: number;
+    props?: QueryFilter<DocumentType>;
+}
+
+export default SearchQuery;
