747e76126f3f1cedb85b11b313b2506e5e4c444f
joaquinrmi <joaquinruaimi@gmail.com>
2022-05-05 21:52:54 -0300

    Add CheepsModel class

---
+++ src/sparrow_model/cheeps.ts
@@ -0,0 +1,82 @@
+import BasicDocument from "../model/basic_document";
+import BasicModel from "../model/basic_model";
+import Schema from "../model/schema/schema";
+
+interface CheepsDocument extends BasicDocument
+{
+    id: number;
+    author_id: number;
+    date_created: number;
+    response_target: number;
+    quote_target: number;
+    content: string;
+    gallery: Array<string>;
+    comments: number;
+    recheeps: number;
+    likes: number;
+}
+
+const cheepsSchema = new Schema<CheepsDocument>("cheeps",
+{
+    id: {
+        type: "serial",
+        primaryKey: true
+    },
+    author_id: {
+        type: "int",
+        notNull: true,
+        references: {
+            column: "id",
+            table: "users"
+        }
+    },
+    date_created: {
+        type: "int",
+        notNull: true
+    },
+    response_target: {
+        type: "int",
+        references: {
+            column: "id",
+            table: "cheeps"
+        }
+    },
+    quote_target: {
+        type: "int",
+        references: {
+            column: "id",
+            table: "cheeps"
+        }
+    },
+    content: {
+        type: "text"
+    },
+    gallery: {
+        type: [ "text" ]
+    },
+    comments: {
+        type: "int",
+        notNull: true,
+        default: 0
+    },
+    recheeps: {
+        type: "int",
+        notNull: true,
+        default: 0
+    },
+    likes: {
+        type: "int",
+        notNull: true,
+        default: 0
+    }
+});
+
+export class CheepsModel extends BasicModel<CheepsDocument>
+{
+    constructor(schema: Schema<CheepsDocument>)
+    {
+        super(schema);
+    }
+}
+
+export default new CheepsModel(cheepsSchema);
