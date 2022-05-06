c87e18b86f06758faa97f49bfa74650c62398c32
joaquinrmi <joaquinruaimi@gmail.com>
2022-05-06 19:57:40 -0300

    Add ErrorType enum

---
+++ src/routes/error_type.ts
@@ -0,0 +1,10 @@
+enum ErrorType
+{
+    InternalServerError = "Internal Server Error",
+    InvalidQuery = "Invalid Query",
+    InvalidForm = "Invalid Form",
+    HandleAlreadyExists = "Username Already Exists",
+    EmailAlreadyExists = "Email Already Exists"
+}
+
+export default ErrorType;
