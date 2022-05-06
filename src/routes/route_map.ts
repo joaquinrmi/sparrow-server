a4215b17c1495673f32e3d2566109477238bc095
joaquinrmi <joaquinruaimi@gmail.com>
2022-05-06 02:10:19 -0300

    Add RouteMap class

---
+++ src/routes/route_map.ts
@@ -0,0 +1,23 @@
+export enum MethodType
+{
+    Get = "get",
+    Post = "post",
+    Put = "put",
+    Delete = "delete"
+}
+
+class RouteMap
+{
+    method: MethodType;
+    path: string;
+    func: string;
+
+    constructor(method: MethodType, path: string, func: string)
+    {
+        this.method = method;
+        this.path = path;
+        this.func = func;
+    }
+}
+
+export default RouteMap;
