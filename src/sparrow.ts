642e2dc9d3b88a54f16f061d35a99c3b5dfce46f
joaquinrmi <joaquinruaimi@gmail.com>
2022-05-06 02:22:01 -0300

    Add Sparrow class

---
+++ src/sparrow.ts
@@ -0,0 +1,59 @@
+import path from "path";
+import express, { Request, Response } from "express";
+import session from "express-session";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import multer from "multer";
+import SparrowModel from "./sparrow_model/";
+
+dotenv.config();
+
+class Sparrow
+{
+    private app: express.Application;
+    private model: SparrowModel;
+    private upload: multer.Multer;
+
+    constructor()
+    {
+        this.app = express();
+        this.model = new SparrowModel();
+
+        const storage = multer.memoryStorage();
+        this.upload = multer({ storage });
+    }
+
+    async start()
+    {
+        try
+        {
+            await this.model.initialize();
+        }
+        catch(err)
+        {
+            throw err;
+        }
+
+        this.app.use(express.json());
+        this.app.use(express.urlencoded({ extended: false }));
+        this.app.use(cookieParser());
+        this.app.use(session({
+            secret: process.env.SESSION_SECRET,
+            resave: false,
+            saveUninitialized: false
+        }));
+
+        this.app.use(express.static(path.join(__dirname, "..", "res")));
+
+        this.app.get("*", (req : Request, res : Response) => {
+            res.sendFile(path.join(__dirname, "..", "res", "index.html"));
+        });
+
+        this.app.listen(process.env.PORT, () =>
+        {
+            console.log(`Server on port ${process.env.PORT}.`);
+        });
+    }
+}
+
+export default Sparrow;
