3d80979b2a72d41481a9e16fa554c91eb5eeeec2
joaquinrmi <joaquinruaimi@gmail.com>
2022-05-06 16:41:45 -0300

    Add StatusCode enum

---
+++ src/routes/status_code.ts
@@ -0,0 +1,13 @@
+enum StatusCode
+{
+   Created = 201,
+   OK = 200,
+   BadRequest = 400,
+   Unauthorized = 401,
+   Forbidden = 403,
+   NotFound = 404,
+   Conflict = 409,
+   InternalServerError = 500
+}
+
+export default StatusCode;
