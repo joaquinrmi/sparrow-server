f3833d7466b4c0745a9ef9d6eacf1ce271333fc7
joaquinrmi <joaquinruaimi@gmail.com>
2022-05-06 19:57:07 -0300

    Add encryption functions

---
+++ src/encryption.ts
@@ -0,0 +1,17 @@
+import CryptoJS from "crypto-js";
+
+export function encrypt(text: string, secret?: string): string
+{
+   return CryptoJS.AES.encrypt(text, secret || process.env.ENCRYPT_SECRET).toString();
+}
+
+export function decrypt(text: string, secret?: string): string
+{
+   const bytes = CryptoJS.AES.decrypt(text, secret || process.env.ENCRYPT_SECRET);
+   return bytes.toString(CryptoJS.enc.Utf8 as any);
+}
+
+export function encryptSHA256(text: string): string
+{
+   return CryptoJS.SHA256(text).toString();
+}
