497226ccf3298898ec8a66d3212b801a7c6533c2
joaquinrmi <joaquinruaimi@gmail.com>
2022-05-10 01:44:40 -0300

    Add 'getTimeline()' to CheepsModel class

--- src/sparrow_model/cheeps.ts
+++ src/sparrow_model/cheeps.ts
@@ -2,6 +2,7 @@ import { Pool } from "pg";
 import BasicDocument from "../model/basic_document";
 import BasicModel from "../model/basic_model";
 import Schema from "../model/schema/schema";
+import FollowsModel from "./follows";
 import ProfilesModel from "./profiles";
 import UsersModel, { UsersDocument } from "./users";
 
@@ -121,6 +122,56 @@ class CheepsModel extends BasicModel<CheepsDocument>
         return documents[0];
     }
 
+    async getTimeline(userId: number, maxTime: number): Promise<Array<CheepData>>
+    {
+        let table = this.getTableName();
+        let cheepColumns = this.columnList.map(column => `${table}.${column}`);
+        let userColumns = [ "users.handle" ];
+        let profileColumns = [ "profiles.name", "profiles.picture" ];
+
+        let columns = [ ...cheepColumns, ...userColumns, ...profileColumns ];
+
+        const query = `SELECT ${columns.join(", ")} FROM follows
+            INNER JOIN cheeps ON cheeps.author = follows.target_id
+            INNER JOIN users ON users.id = follows.target_id
+            INNER JOIN profiles ON profiles.user_id = follows.target_id
+            WHERE follows.user_id = $1 AND cheeps.date_created < $2
+            ORDER BY cheeps.date_created LIMIT 20;`;
+
+        try
+        {
+            var response = await this.pool.query(query, [ userId, maxTime ]);
+        }
+        catch(err)
+        {
+            throw err;
+        }
+
+        let result = new Array<CheepData>();
+
+        for(let i = 0; i < response.rowCount; ++i)
+        {
+            result.push({
+                id: response.rows[i].id,
+                author: {
+                    handle: response.rows[i].handle,
+                    name: response.rows[i].name,
+                    picture: response.rows[i].picture,
+                },
+                dateCreated: response.rows[i].date_created,
+                responseTarget: response.rows[i].response_target,
+                quoteTarget: response.rows[i].quote_target,
+                content: response.rows[i].content,
+                gallery: response.rows[i].gallery,
+                comments: response.rows[i].comments,
+                likes: response.rows[i].likes,
+                recheeps: response.rows[i].recheeps
+            });
+        }
+
+        return result;
+    }
+
     async registerNewComment(cheepId: number): Promise<void>
     {
         try
@@ -189,6 +240,24 @@ class CheepsModel extends BasicModel<CheepsDocument>
             throw err;
         }
     }
+}
+
+export interface CheepData
+{
+    id: number;
+    author: {
+        handle: string;
+        name: string;
+        picture: string;
+    };
+    dateCreated: number;
+    responseTarget: number;
+    quoteTarget: number;
+    content: string;
+    gallery: Array<string>;
+    comments: number;
+    likes: number;
+    recheeps: number;
 }
 
 export default CheepsModel;
