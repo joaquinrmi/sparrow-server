33e06e15b39765756f8c0512ddc3a624a8bda58d
joaquinrmi <joaquinruaimi@gmail.com>
2022-05-11 23:56:58 -0300

    Add 'getFollowers()' to FollowsModel class

--- src/sparrow_model/follows.ts
+++ src/sparrow_model/follows.ts
@@ -2,6 +2,7 @@ import { Pool } from "pg";
 import BasicDocument from "../model/basic_document";
 import BasicModel from "../model/basic_model";
 import Schema from "../model/schema/schema";
+import UserCellInfo from "./user_cell_info";
 
 export interface FollowsDocument extends BasicDocument
 {
@@ -54,6 +55,79 @@ class FollowsModel extends BasicModel<FollowsDocument>
         {
             throw err;
         }
+    }
+
+    async getFollowers(currentUserId: number, userId: number, offset: number): Promise<{
+        users: Array<UserCellInfo>,
+        offset: number
+    }>
+    {
+        const query = `
+            SELECT follows.id, users.id AS user_id, users.handle, profiles.name, profiles.picture, profiles.description
+            FROM follows
+            INNER JOIN users ON users.id = follows.target_id
+            INNER JOIN profiles ON profiles.id = users.profile_id
+            WHERE follows.user_id = $1 AND follows.id < $2
+            LIMIT 20;
+        `;
+
+        try
+        {
+            var response = await this.pool.query(query, [ userId, offset ]);
+        }
+        catch(err)
+        {
+            throw err;
+        }
+
+        let users = new Array<UserCellInfo>();
+
+        if(response.rowCount === 0)
+        {
+            return {
+                users: users,
+                offset: Number.MAX_SAFE_INTEGER
+            }
+        }
+
+        for(let i = 0; i < response.rowCount; ++i)
+        {
+            users.push(await this.createUserCellInfo(response.rows[i], currentUserId));
+        }
+
+        let newOffset = response.rows[response.rowCount - 1].id;
+
+        return {
+            users: users,
+            offset: newOffset
+        };
+    }
+
+    private async createUserCellInfo(row: any, userId: number): Promise<UserCellInfo>
+    {
+        try
+        {
+            var exists = await this.exists({
+                props: [
+                    {
+                        user_id: userId,
+                        target_id: row.user_id
+                    }
+                ]
+            });
+        }
+        catch(err)
+        {
+            throw err;
+        }
+
+        return {
+            handle: row.handle,
+            name: row.name,
+            picture: row.picture,
+            description: row.description,
+            following: exists
+        };
     }
 }
 
