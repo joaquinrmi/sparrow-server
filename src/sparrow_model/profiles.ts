57b7decdf619ef5e596e1925230d51a46fe3d7b5
joaquinrmi <joaquinruaimi@gmail.com>
2022-05-12 03:25:51 -0300

    Add 'edit()' to ProfilesModel class

--- src/sparrow_model/profiles.ts
+++ src/sparrow_model/profiles.ts
@@ -2,6 +2,7 @@ import { Pool } from "pg";
 import BasicDocument from "../model/basic_document";
 import BasicModel from "../model/basic_model";
 import Schema from "../model/schema/schema";
+import EditProfileForm from "../routes/profiles/edit_profile_form";
 import UsersModel from "./users";
 
 export interface ProfilesDocument extends BasicDocument
@@ -82,6 +83,33 @@ class ProfilesModel extends BasicModel<ProfilesDocument>
         try
         {
             await this.pool.query(`UPDATE ${this.getTableName()} SET cheeps = cheeps + 1 FROM ${this.getTableName()} a INNER JOIN ${usersModel.getTableName()} b ON b.id = $1 AND a.id = b.profile_id`, [ userId ]);
+        }
+        catch(err)
+        {
+            throw err;
+        }
+    }
+
+    async edit(userId: number, data: EditProfileForm): Promise<void>
+    {
+        let columns = new Array<string>();
+        let values = new Array<any>();
+
+        let index = 1;
+        for(let column in data)
+        {
+            columns.push(`profiles.${column} = $${index}`);
+            values.push(data[column]);
+            ++index;
+        }
+
+        values.push(userId);
+
+        const query = `UPDATE profiles SET ${columns.join(", ")} FROM profiles INNER JOIN users ON profiles.id = users.profile_id AND users.id = $${values.length};`;
+
+        try
+        {
+            await this.pool.query(query, values);
         }
         catch(err)
         {
