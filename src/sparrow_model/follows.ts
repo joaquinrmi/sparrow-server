90be0509b6e6229ebeb283fd4a5ad1422569c4af
joaquinrmi <joaquinruaimi@gmail.com>
2022-05-12 00:01:57 -0300

    Add 'getFollowing()' to FollowsModel

--- src/sparrow_model/follows.ts
+++ src/sparrow_model/follows.ts
@@ -103,6 +103,52 @@ class FollowsModel extends BasicModel<FollowsDocument>
         };
     }
 
+    async getFollowing(currentUserId: number, userId: number, offset: number): Promise<{
+        users: Array<UserCellInfo>,
+        offset: number
+    }>
+    {
+        const query = `
+            SELECT follows.id, users.id AS user_id, users.handle, profiles.name, profiles.picture, profiles.description
+            FROM follows
+            INNER JOIN users ON users.id = follows.target_id
+            INNER JOIN profiles ON profiles.id = users.profile_id
+            WHERE follows.user_id = $1 AND follows.id < $2
+            LIMIT 20;
+        `;
+
+        try
+        {
+            var response = await this.pool.query(query, [ userId, offset ]);
+        }
+        catch(err)
+        {
+            throw err;
+        }
+
+        let users = new Array<UserCellInfo>();
+
+        if(response.rowCount === 0)
+        {
+            return {
+                users: users,
+                offset: Number.MAX_SAFE_INTEGER
+            }
+        }
+
+        for(let i = 0; i < response.rowCount; ++i)
+        {
+            users.push(await this.createUserCellInfo(response.rows[i], currentUserId));
+        }
+
+        let newOffset = response.rows[response.rowCount - 1].id;
+
+        return {
+            users: users,
+            offset: newOffset
+        };
+    }
+
     private async createUserCellInfo(row: any, userId: number): Promise<UserCellInfo>
     {
         try
