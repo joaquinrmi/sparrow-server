30e2c2523deb78fb35a8ab698c1059f2eb07ab0c
joaquinrmi <joaquinruaimi@gmail.com>
2022-06-12 04:03:37 -0300

    Fix error in UsersModel.createNewUser()

--- src/sparrow_model/users.ts
+++ src/sparrow_model/users.ts
@@ -80,7 +80,7 @@ class UsersModel extends BasicModel<UsersDocument>
 
             const imageKeeper = new ImageKeeper(userDocument.id);
             const defaultPicPath = path.join(__dirname, "..", "..", "img", "profile_default.png");
-            const profilePic = await imageKeeper.saveImage(defaultPicPath);
+            const profilePic = await imageKeeper.saveProfilePicture(defaultPicPath);
 
             await this.model.profilesModel.update(
                 {
