7aa31a13e53284ae3fa95444b24b2ff3621f57c7
joaquinrmi <joaquinruaimi@gmail.com>
2022-06-19 05:10:33 -0300

    Add 'getUserProfileData()' to ProfilesModel class

--- src/sparrow_model/profiles.ts
+++ src/sparrow_model/profiles.ts
@@ -83,7 +83,52 @@ class ProfilesModel extends BasicModel<ProfilesDocument>
         this.model = model;
     }
 
+    async getUserProfileData(currentUserId: number, userHandle: string): Promise<UserProfileData>
+    {
+        const followingQuery = `SELECT count(f.id) FROM follows AS f INNER JOIN users AS u ON f.target_id = u.id WHERE f.user_id = $1 AND u.handle = $2`;
 
+        const query = `SELECT ${this.columnList.map(column => `p.${column}`).join(", ")}, (${followingQuery} > 0) AS user_follows_them FROM profiles AS p INNER JOIN users AS u ON p.id = u.profile_id WHERE u.handle = $2`;
+
+        try
+        {
+            var response = await this.pool.query(query, [ currentUserId, userHandle ]);
+        }
+        catch(err)
+        {
+            throw err;
+        }
+
+        if(response.rowCount === 0)
+        {
+            return null;
+        }
+
+        const currentUser = await this.model.usersModel.exists({
+            props: [
+                {
+                    id: currentUserId,
+                    handle: userHandle
+                }
+            ]
+        });
+
+        const profileData = response.rows[0];
+
+        return {
+            name: profileData.name,
+            picture: profileData.picture,
+            banner: profileData.banner === null ? undefined : profileData.banner,
+            description: profileData.description === null ? undefined : profileData.description,
+            joinDate: profileData.join_date,
+            birthdate: currentUser ? profileData.birthdate : undefined,
+            location: profileData.location,
+            website: profileData.website,
+            followingCount: profileData.following,
+            followerCount: profileData.followers,
+            cheepCount: profileData.cheeps,
+            following: profileData.user_follows_them,
+        };
+    }
 
     async registerNewCheep(userId: number): Promise<void>
     {
@@ -193,7 +238,7 @@ export interface UserProfileData
     banner?: string;
     description?: string;
     joinDate: number;
-    birthdate: number;
+    birthdate?: number;
     location: string;
     website: string;
     followingCount: number;
