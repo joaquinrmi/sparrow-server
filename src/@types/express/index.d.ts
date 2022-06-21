0951f2552cc324e727decba04396c29679979b63
joaquinrmi <joaquinruaimi@gmail.com>
2022-06-21 10:47:21 -0300

    Add 'searchUsersForm' to Request interface

--- src/@types/express/index.d.ts
+++ src/@types/express/index.d.ts
@@ -3,6 +3,7 @@ import EditProfileForm from "../../routes/profiles/edit_profile_form";
 import CreateUserForm from "../../routes/users/create_user_form";
 import LoginForm from "../../routes/users/login_form";
 import SparrowModel from "../../sparrow_model/";
+import SearchUsersForm from "../../routes/users/search_users_form";
 
 declare global
 {
@@ -15,6 +16,7 @@ declare global
             loginForm: LoginForm;
             newCheepForm: NewCheepForm;
             editProfileForm: EditProfileForm;
+            searchUsersForm: SearchUsersForm;
         }
     }
 }
