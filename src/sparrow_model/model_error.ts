9d9d17e31daf5685f912c2fcca2b05491016c7c3
joaquinrmi <joaquinruaimi@gmail.com>
2022-06-25 10:20:35 -0300

    Add ModelError class

---
+++ src/sparrow_model/model_error.ts
@@ -0,0 +1,16 @@
+class ModelError
+{
+    type: ModelErrorType;
+    message?: string;
+
+    constructor(type: ModelErrorType, message?: string)
+    {
+        this.type = type;
+        this.message = message;
+    }
+}
+
+export enum ModelErrorType
+{}
+
+export default ModelError;
